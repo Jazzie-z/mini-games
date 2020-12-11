c4756e04ba0e2860bafca14a01435d3859e1866b
Jaikumar.T.J <tjjaikumar@gmail.com>
2020-12-11 13:25:13 +0530

    refactor(prettier-ignore)

--- src/games/SpaceInvaders.js
+++ src/games/SpaceInvaders.js
@@ -51,6 +51,7 @@ const SpaceInvaders = () => {
   const [laserIndex, setLaserIndex] = useState("");
   const [start, setStart] = useState(false);
   useEffect(() => {
+    // prettier-ignore
     let alienLayout = [
       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20,
       21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 40,
